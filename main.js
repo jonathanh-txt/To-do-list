5a804f5b98c266f1d8a6db7b92447d470cc0f698
jonathanh-txt <jonathanhardy.hd@gmail.com>
2026-07-28 22:21:15 +0700

    add done button and add done counter

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const input = document.getElementById("todoInput");
 const addButton = document.getElementById("add");
 const todoList = document.getElementById("todoList");
+let doneList = 0;
+const doneCount = document.getElementById("donecount");
 addButton.addEventListener("click", function () {
   if (input.value.trim() === "") {
     alert("input tidak boleh kosong");
@@ -9,6 +11,17 @@ addButton.addEventListener("click", function () {
     const toDoItem = document.createElement("div");
     toDoItem.classList.add("todoitem");
     todoList.append(toDoItem);
+    /done button/;
+    const divDoneButton = document.createElement("div");
+    divDoneButton.classList.add("divdonebutton");
+    toDoItem.append(divDoneButton);
+    const doneButton = document.createElement("button");
+    doneButton.classList.add("donebutton");
+    divDoneButton.append(doneButton);
+    const doneImg = document.createElement("img");
+    doneImg.classList.add("imgdonebutton");
+    doneButton.append(doneImg);
+    doneImg.src = "/Project to do list/assets/check.svg";
     /list/;
     const newList = document.createElement("div");
     console.log(input.value);
@@ -26,11 +39,19 @@ addButton.addEventListener("click", function () {
     DivDelButton.append(delButton);
     /img trash/;
     const imgDel = document.createElement("img");
-    imgDel.classList.add("imgDel");
+    imgDel.classList.add("imgdelbutton");
     delButton.append(imgDel);
-    imgDel.src = "/Project to do list/assets/trash.png";
+    imgDel.src = "/Project to do list/assets/trash.svg";
+
     delButton.addEventListener("click", function () {
       toDoItem.remove();
+    });
+
+    doneButton.addEventListener("click", function () {
+      toDoItem.remove();
+      doneList = doneList + 1;
+      doneCount.textContent = doneList;
+      console.log(doneList);
     });
   }
 });
